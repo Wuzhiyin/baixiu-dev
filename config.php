1473f9a9f33d064b6381c9e1565b32e8b323983e
Wuzhiyin <34057023+Wuzhiyin@users.noreply.github.com>
2020-09-04 19:07:24 +0800

    step-03 项目配置文件

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 项目配置文件
+ */
+
+/**
+ * 数据库主机
+ */
+define('DB_HOST', '127.0.0.1');
+
+/**
+ * 数据库用户名
+ */
+define('DB_USER', 'root');
+
+/**
+ * 数据库密码
+ */
+define('DB_PASS', '123');
+
+/**
+ * 数据库名称
+ */
+define('DB_NAME', 'baixiu-dev');
+
+/**
+ * 设置网站根目录
+ */
+define('ROOT_DIR', dirname(__FILE__));
--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 /**
  * 网站前台脚本文件
  */
+ require_once 'config.php';
 
+ // echo ROOT_DIR;
 ?>
 <h1> 前台 </h1>
+<a href="/admin/"> 后台 </a>
