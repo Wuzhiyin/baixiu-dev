393d401403c689decddfd85c963254c572599a92
Wuzhiyin <34057023+Wuzhiyin@users.noreply.github.com>
2020-09-06 08:26:03 +0800

    step-07 处理静态HTML中需要调整的地方

--- admin/login.php
+++ admin/login.php
@@ -4,11 +4,14 @@
   <meta charset="utf-8">
   <title>Sign in &laquo; Admin</title>
   <link rel="stylesheet" href="/static/assets/vendors/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="/static/assets/vendors/animate/animate.css">
   <link rel="stylesheet" href="/static/assets/css/admin.css">
 </head>
 <body>
   <div class="login">
-    <form class="login-wrap">
+    <!-- 可以通过在 form 上添加 novalidate 取消浏览器自带的校验功能 -->
+    <!-- autocomplete="off" 关闭客户端的自动完成功能 -->
+    <form class="login-wrap" action="/admin/login.php" method="post" autocomplete="off" novalidate>
       <img class="avatar" src="/static/assets/img/default.png">
       <!-- 有错误信息时展示 -->
       <!-- <div class="alert alert-danger">
@@ -16,13 +19,13 @@
       </div> -->
       <div class="form-group">
         <label for="email" class="sr-only">邮箱</label>
-        <input id="email" type="email" class="form-control" placeholder="邮箱" autofocus>
+        <input id="email" name="email" type="email" class="form-control" placeholder="邮箱" autofocus>
       </div>
       <div class="form-group">
         <label for="password" class="sr-only">密码</label>
-        <input id="password" type="password" class="form-control" placeholder="密码">
+        <input id="password" name="password" type="password" class="form-control" placeholder="密码">
       </div>
-      <a class="btn btn-primary btn-block" href="index.php">登 录</a>
+      <button class="btn btn-primary btn-block" type="submit">登 录</button>
     </form>
   </div>
 </body>
